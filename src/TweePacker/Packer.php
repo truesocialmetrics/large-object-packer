84a1e27da2700b7d7b033dc0f6915d4d45083784
Rostyslav Mykhajliw <necromant2005@gmail.com>
2023-11-10 12:32:44 +0200

    remove packer class

--- src/TweePacker/Packer.php
+++ src/TweePacker/Packer.php
@@ -1,442 +1,60 @@
 <?php
-namespace TweePacker\Storage\Adapter;
-
+namespace TweePacker;
 use Laminas\Cache\Storage\StorageInterface;
 use Laminas\Cache\Storage\TaggableInterface;
-use Laminas\Cache\Storage\FlushableInterface;
-use Laminas\Cache\Exception\UnsupportedMethodCallException;
-use Laminas\Cache\Storage\Capabilities;
-use stdClass;
+use TweePacker\Utils\Pack as UtilsPack;
 
-use MongoDB\BSON\UTCDateTime;
-use MongoDB\Client as SystemMongoDBClient;
-
-class MongoDb implements StorageInterface, TaggableInterface
+class Packer
 {
-    private const FIELD_KEY = '_id';
-    private const FIELD_VALUE = 'value';
-    private const FIELD_TAGS = 'tags';
-    private const FIELD_DATESTAMP = 'created_at';
+    const MAX_ITEM_SIZE = 300000;
 
-    private $options = [];
+    protected $storage = null;
 
-    private $collection = null;
+    protected $maxItemSize = self::MAX_ITEM_SIZE;
 
-    public function __construct(array $options)
+    public function __construct(StorageInterface&TaggableInterface $storage, $maxItemSize = self::MAX_ITEM_SIZE)
     {
-        $this->options = $options;
+        $this->storage = $storage;
+        $this->maxItemSize = $maxItemSize;
     }
 
-    private function getCollection()
+    public function setItem($key, $value, array $tags = []) : void
     {
-        if ($this->collection) {
-            return $this->collection;
+        $value = serialize($value);
+        if (strlen($value) < $this->maxItemSize) {
+            $this->storage->setItem($key, $value);
+            $this->storage->setTags($key, $tags);
+            return;
         }
-        $client = new SystemMongoDBClient($this->options['uri'],
-            $this->options['uriOptions'],
-            $this->options['driverOptions']);
-        $db = $client->selectDatabase($this->options['database']);
-        $this->collection = $db->selectCollection($this->options['collection']);
-
-        return $this->collection;
-    }
-
-    /**
-     * Set options.
-     *
-     * @param array|Traversable|Adapter\AdapterOptions $options
-     * @return StorageInterface Fluent interface
-     */
-    public function setOptions($options)
-    {
-        $this->options = $options;
-    }
-
-    /**
-     * Get options
-     *
-     * @return Adapter\AdapterOptions
-     */
-    public function getOptions()
-    {
-        return $this->options;
-    }
-
-    /* reading */
-
-    /**
-     * Get an item.
-     *
-     * @param  string  $key
-     * @param  bool $success
-     * @param  mixed   $casToken
-     * @return mixed Data on success, null on failure
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function getItem($key, & $success = null, & $casToken = null)
-    {
-        $item = $this->getCollection()->findOne([self::FIELD_KEY => $key]);
-        if (!$item) {
-            return null;
+        $pack = new UtilsPack();
+        for ($i = 0; $i < strlen($value) / $this->maxItemSize; $i++) {
+            $_key   = $key . '::' . $i;
+            $_value = substr($value, $i * $this->maxItemSize, $this->maxItemSize);
+            $pack->append($_key);
+            $this->storage->setItem($_key, $_value);
+            $this->storage->setTags($_key, $tags);
         }
-
-        return $item[self::FIELD_VALUE];
+        $this->storage->setItem($key, $pack);
+        $this->storage->setTags($key, $tags);
     }
 
-    /**
-     * Get multiple items.
-     *
-     * @param  array $keys
-     * @return array Associative array of keys and values
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function getItems(array $keys)
+    public function getItem($key)
     {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Test if an item exists.
-     *
-     * @param  string $key
-     * @return bool
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function hasItem($key)
-    {
-        $item = $this->getCollection()->findOne([self::FIELD_KEY, $key]);
-        if (!$item) {
-            return false;
+        $raw = $this->storage->getItem($key);
+        if ($raw instanceof UtilsPack) {
+            $value = '';
+            foreach ($raw as $_key) {
+                $_value = $this->storage->getItem($_key);
+                $value .= $_value;
+            }
+        } else {
+            $value = $raw;
         }
-
-        return true;
+        return unserialize($value);
     }
 
-    /**
-     * Test multiple items.
-     *
-     * @param  array $keys
-     * @return array Array of found keys
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function hasItems(array $keys)
+    public function clearByTags(array $tags)
     {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Get metadata of an item.
-     *
-     * @param  string $key
-     * @return array|bool Metadata on success, false on failure
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function getMetadata($key)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Get multiple metadata
-     *
-     * @param  array $keys
-     * @return array Associative array of keys and metadata
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function getMetadatas(array $keys)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /* writing */
-
-    /**
-     * Store an item.
-     *
-     * @param  string $key
-     * @param  mixed  $value
-     * @return bool
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function setItem($key, $value)
-    {
-        $this->getCollection()->updateOne([self::FIELD_KEY => $key],
-            ['$set' => [
-                self::FIELD_VALUE => $value,
-                self::FIELD_DATESTAMP => new UTCDateTime(),
-            ]],
-            ['upsert' => true, 'w' => 1]);
-    }
-
-    /**
-     * Store multiple items.
-     *
-     * @param  array $keyValuePairs
-     * @return array Array of not stored keys
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function setItems(array $keyValuePairs)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Add an item.
-     *
-     * @param  string $key
-     * @param  mixed  $value
-     * @return bool
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function addItem($key, $value)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Add multiple items.
-     *
-     * @param  array $keyValuePairs
-     * @return array Array of not stored keys
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function addItems(array $keyValuePairs)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Replace an existing item.
-     *
-     * @param  string $key
-     * @param  mixed  $value
-     * @return bool
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function replaceItem($key, $value)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Replace multiple existing items.
-     *
-     * @param  array $keyValuePairs
-     * @return array Array of not stored keys
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function replaceItems(array $keyValuePairs)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Set an item only if token matches
-     *
-     * It uses the token received from getItem() to check if the item has
-     * changed before overwriting it.
-     *
-     * @param  mixed  $token
-     * @param  string $key
-     * @param  mixed  $value
-     * @return bool
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     * @see    getItem()
-     * @see    setItem()
-     */
-    public function checkAndSetItem($token, $key, $value)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Reset lifetime of an item
-     *
-     * @param  string $key
-     * @return bool
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function touchItem($key)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Reset lifetime of multiple items.
-     *
-     * @param  array $keys
-     * @return array Array of not updated keys
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function touchItems(array $keys)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Remove an item.
-     *
-     * @param  string $key
-     * @return bool
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function removeItem($key)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Remove multiple items.
-     *
-     * @param  array $keys
-     * @return array Array of not removed keys
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function removeItems(array $keys)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Increment an item.
-     *
-     * @param  string $key
-     * @param  int    $value
-     * @return int|bool The new value on success, false on failure
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function incrementItem($key, $value)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Increment multiple items.
-     *
-     * @param  array $keyValuePairs
-     * @return array Associative array of keys and new values
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function incrementItems(array $keyValuePairs)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Decrement an item.
-     *
-     * @param  string $key
-     * @param  int    $value
-     * @return int|bool The new value on success, false on failure
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function decrementItem($key, $value)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /**
-     * Decrement multiple items.
-     *
-     * @param  array $keyValuePairs
-     * @return array Associative array of keys and new values
-     * @throws \Laminas\Cache\Exception\ExceptionInterface
-     */
-    public function decrementItems(array $keyValuePairs)
-    {
-        throw new UnsupportedMethodCallException(__METHOD__ . ' is not suported');
-    }
-
-    /* status */
-
-    /**
-     * Capabilities of this storage
-     *
-     * @return Capabilities
-     */
-    public function getCapabilities()
-    {
-        return $this->capabilities  = new Capabilities(
-            $this,
-            new stdClass(),
-            [
-                'supportedDatatypes' => [
-                    'NULL'     => true,
-                    'boolean'  => true,
-                    'integer'  => true,
-                    'double'   => true,
-                    'string'   => true,
-                    'array'    => true,
-                    'object'   => false,
-                    'resource' => false,
-                ],
-                'supportedMetadata'  => [
-                    '_id',
-                ],
-                'minTtl'             => 1,
-                'staticTtl'          => true,
-                'maxKeyLength'       => 255,
-                'namespaceIsPrefix'  => true,
-            ]
-        );
-    }
-
-    /**
-     * Set tags to an item by given key.
-     * An empty array will remove all tags.
-     *
-     * @param string   $key
-     * @param string[] $tags
-     * @return bool
-     */
-    public function setTags($key, array $tags)
-    {
-        $this->getCollection()->updateOne([self::FIELD_KEY => $key],
-            ['$set' => [
-                self::FIELD_TAGS => $tags,
-            ]],
-            ['upsert' => true, 'w' => 1]);
-    }
-
-    /**
-     * Get tags of an item by given key
-     *
-     * @param string $key
-     * @return string[]|FALSE
-     */
-    public function getTags($key)
-    {
-        $item = $this->getCollection()->findOne([self::FIELD_KEY, $key]);
-        if (!$item) {
-            return false;
-        }
-
-        return $item[self::FIELD_TAGS];
-    }
-
-    /**
-     * Remove items matching given tags.
-     *
-     * If $disjunction only one of the given tags must match
-     * else all given tags must match.
-     *
-     * @param string[] $tags
-     * @param  bool  $disjunction
-     * @return bool
-     */
-    public function clearByTags(array $tags, $disjunction = false)
-    {
-        // tags: { $in: ['red', 'blank'] }
-        $this->getCollection()->deleteMany(['tags' => ['$in' => $tags]]);
-    }
-
-    /**
-     * Flush the whole storage
-     *
-     * @return bool
-     */
-    public function flush()
-    {
-        $this->getCollection()->deleteMany([]);
+        $this->storage->clearByTags($tags, true);
     }
 }
